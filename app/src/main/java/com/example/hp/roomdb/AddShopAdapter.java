ee3d68dcc23aff5fd08d1a1cdda3fb622c03d0fb
Yuri Yurkov <yu.yurkov@gmail.com>
2018-07-11 20:25:26 +1000

    addshop

---
+++ app/src/main/java/com/example/hp/roomdb/AddShopAdapter.java
@@ -0,0 +1,49 @@
+package com.example.hp.roomdb;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AddShopAdapter extends RecyclerView.Adapter<AddShopAdapter.ViewHolder> {
+
+    ArrayList<String> shops;
+
+    public AddShopAdapter(ArrayList<String> shops) {
+        this.shops = shops;
+    }
+
+    @NonNull
+    @Override
+    public AddShopAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.shop_row,parent,false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AddShopAdapter.ViewHolder holder, int position) {
+        holder.shop_title.setText(shops.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return shops.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView shop_title;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            shop_title = itemView.findViewById(R.id.shop_title);
+        }
+    }
+}
--- app/src/main/java/com/example/hp/roomdb/MainActivity.java
+++ app/src/main/java/com/example/hp/roomdb/MainActivity.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
     private static final String TAG = "MainActivity";
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+
+    ArrayList<String> shops;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddShop.class));
             }
         });
+
+
+        shops = new ArrayList<>();
+
+        for (int i = 0; i < 10 ; i++) {
+            shops.add("Магазин: " + i);
+        }
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AddShopAdapter(shops);
+        recyclerView.setAdapter(adapter);
 
     }
 
