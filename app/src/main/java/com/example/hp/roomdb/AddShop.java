cc54a212d8487157b9c22e2e799a642e604f9fa2
Yuri Yurkov <yu.yurkov@gmail.com>
2018-07-11 19:53:11 +1000

    addshop

---
+++ app/src/main/java/com/example/hp/roomdb/AddShop.java
@@ -0,0 +1,50 @@
+package com.example.hp.roomdb;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddShop extends AppCompatActivity {
+
+
+    EditText shop_address;
+    EditText shop_title;
+    EditText shop_tel;
+    EditText contact_name;
+    EditText worktime;
+    Button button;
+
+    private static final String TAG = "AddShop";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_shop);
+
+        shop_address = findViewById(R.id.shop_address);
+        shop_title = findViewById(R.id.shop_title);
+        shop_tel = findViewById(R.id.shop_tel);
+        contact_name = findViewById(R.id.contact_name);
+        worktime = findViewById(R.id.worktime);
+        button = findViewById(R.id.save_input_data);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: save to database
+                Log.d(TAG, "onClick:"
+                        + shop_address.getText().toString()
+                        + "\n" + shop_title.getText().toString()
+                       + "\n" + shop_tel.getText().toString()
+                        + "\n" + contact_name.getText().toString()
+                        + "\n" + worktime.getText().toString()
+                );
+            }
+        });
+    }
+}
+
--- app/src/main/java/com/example/hp/roomdb/MainActivity.java
+++ app/src/main/java/com/example/hp/roomdb/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.hp.roomdb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: pressed!");
-                startActivity(new Intent(MainActivity.this, CreateUser.class));
+                startActivity(new Intent(MainActivity.this, AddShop.class));
             }
         });
 
