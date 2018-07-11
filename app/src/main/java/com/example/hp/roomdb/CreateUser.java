acad9dc3ab176cc051dd6ef3225f75ccdf9459ac
Yuri Yurkov <yu.yurkov@gmail.com>
2018-07-11 13:46:35 +1000

    initial commit

---
+++ app/src/main/java/com/example/hp/roomdb/CreateUser.java
@@ -0,0 +1,14 @@
+package com.example.hp.roomdb;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+    }
+}
+
