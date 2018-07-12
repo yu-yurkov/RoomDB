6dab1a2b116831f21fd7aa919631b8f22898b819
Yuri Yurkov <yu.yurkov@gmail.com>
2018-07-12 15:17:05 +1000

    room

---
+++ app/src/main/java/com/example/hp/roomdb/ShopAddress.java
@@ -0,0 +1,43 @@
+package com.example.hp.roomdb;
+
+public class ShopAddress {
+
+    private int id;
+    private String address;
+    private String title;
+    private String tel;
+    private String contact_name;
+    private String worktime;
+
+    public ShopAddress(String address, String title, String tel, String contact_name, String worktime) {
+        this.address = address;
+        this.title = title;
+        this.tel = tel;
+        this.contact_name = contact_name;
+        this.worktime = worktime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getContact_name() {
+        return contact_name;
+    }
+
+    public String getWorktime() {
+        return worktime;
+    }
+}
